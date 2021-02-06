4c973359ced875015bf3f2df53e3474ba6accdce
Avinash <Avinashkumarsinha007@gmail.com>
2021-02-06 16:28:00 +0530

    vesion 1.1

--- javascript/script.js
+++ javascript/script.js
@@ -24,10 +24,6 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
-
-
-
-
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("shell");
@@ -44,20 +40,30 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
-function showSlides(n) {
+var slideIndex1 = 1;
+showSlides1(slideIndex1);
+
+
+
+function currentSlide1(n) {
+  showSlides1(slideIndex1 = n);
+}
+
+
+function showSlides1(n) {
     var i;
     var slides = document.getElementsByClassName("show11");
     var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}    
-    if (n < 1) {slideIndex = slides.length}
+    if (n > slides.length) {slideIndex1 = 1}    
+    if (n < 1) {slideIndex1 = slides.length}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
+    slides[slideIndex1-1].style.display = "block";  
+    dots[slideIndex1-1].className += " active";
   }
 
 //   function showSlides(n) {
@@ -121,3 +127,5 @@ function show1() {
   y[ab-1].style.color = " rgb(243, 104, 243)" ;
   
 }
+
+
